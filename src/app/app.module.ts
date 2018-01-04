290db83158313c9939411c9234a10d77ea6ff433
abhineetsharma <asharm28@binghamton.edu>
2018-01-03 21:36:59 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
